97cb2609b80542eb8f1f1163e8fd72385293aed1
AnhHNPTIT <anhhn97.ptit@gmail.com>
2020-07-04 14:53:02 +0700

    Login by Facebook

---
+++ fb-init.php
@@ -0,0 +1,46 @@
+<?php
+
+//start the session
+session_start();
+
+//include autoload file from vendor folder
+require './vendor/autoload.php';
+
+
+$fb = new Facebook\Facebook([
+	'app_id' => '1520933634753259',
+	'app_secret' => '418cdbc83a01550a03edfe1c2f9f87c3',
+    'default_graph_version' => 'v2.7'
+        ]);
+
+
+$helper = $fb->getRedirectLoginHelper();
+$login_url = $helper->getLoginUrl("http://localhost:8081/fb-login/");
+
+try {
+
+    $accessToken = $helper->getAccessToken();
+    if (isset($accessToken)) {
+        $_SESSION['access_token'] = (string) $accessToken;  //conver to string
+        //if session is set we can redirect to the user to any page 
+        header("Location:index.php");
+    }
+} catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+}
+
+
+//now we will get users first name , email , last name
+if (isset($_SESSION['access_token'])) {
+
+    try {
+
+        $fb->setDefaultAccessToken($_SESSION['access_token']);
+        $res = $fb->get('/me?locale=en_US&fields=name,email');
+        $user = $res->getGraphUser();
+        echo 'Hello, ',$user->getField('name');
+        
+    } catch (Exception $exc) {
+        echo $exc->getTraceAsString();
+    }
+}
